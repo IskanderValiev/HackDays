f8abfcb0a13946c3ad035cb40456be54bad98273
GaliFFun <galiffun@gmail.com>
2018-09-21 09:08:55 +0300

    added redis sessions, caching support

--- src/main/java/ru/itis/Application.java
+++ src/main/java/ru/itis/Application.java
@@ -2,9 +2,11 @@ package ru.itis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
+@EnableCaching
 public class Application {
 
     private static ConfigurableApplicationContext applicationContext;
